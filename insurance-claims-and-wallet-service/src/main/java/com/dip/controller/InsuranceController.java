0c580cfd70ad2bc9baed761acf2eba3f25afd6a2
ANIK8-JHA <aniketjha0057@outlook.com>
2024-05-06 13:57:28 +0530

    Added the apis related to purchasing and fetching insurances.

--- insurance-claims-and-wallet-service/src/main/java/com/dip/controller/InsuranceController.java
+++ insurance-claims-and-wallet-service/src/main/java/com/dip/controller/InsuranceController.java
@@ -1,5 +1,33 @@
 package com.dip.controller;
 
-public class InsuranceController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dip.model.Insurances;
+import com.dip.service.InsuranceService;
+
+@RestController
+@RequestMapping("/dip")
+public class InsuranceController {
+	
+	@Autowired
+	private InsuranceService insuranceService;
+	
+	@PostMapping("/buy-insurance/{username}")
+	public Insurances buyInsurance(@RequestHeader("Authoriazation") @PathVariable String username, @RequestBody Insurances insurance) {
+		return insuranceService.purchasePolicy(insurance, username);
+	}
+	
+	@GetMapping("/get-insurances/{username}")
+	public List<Insurances> getInsuranceHistory(@PathVariable String username) {
+		return insuranceService.InsuranceHistory(username);
+	}
 }
