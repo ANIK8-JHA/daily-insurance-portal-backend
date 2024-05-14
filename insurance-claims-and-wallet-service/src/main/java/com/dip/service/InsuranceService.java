b491dfdbec2e02997a1531415c5eadf005061d09
ANIK8-JHA <aniketjha0057@outlook.com>
2024-05-14 14:25:29 +0530

    Made small changes in Insurance service part

--- insurance-claims-and-wallet-service/src/main/java/com/dip/service/InsuranceService.java
+++ insurance-claims-and-wallet-service/src/main/java/com/dip/service/InsuranceService.java
@@ -9,24 +9,41 @@ import org.springframework.stereotype.Service;
 import com.dip.model.Insurances;
 import com.dip.repository.InsuranceRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class InsuranceService {
 	
 	@Autowired
 	private InsuranceRepository insuranceRepo;
 	
-	public Insurances purchasePolicy(Insurances policy, String username) {
+	@Autowired
+	private WalletService walletService;
+	
+	
+	public Insurances purchasePolicy(Insurances policy, String username) throws Exception {
+		log.info("checking if user has sufficient wallet balance or not");
+		if(this.walletService.getCurrentWalletBalanceByUsername(username) < policy.getPremium()) {
+			log.info("balance is less than the required balance");
+			throw new Exception("Not enough wallet balance");
+		}
+		log.info("sufficent balance found : " + this.walletService.getCurrentWalletBalanceByUsername(username));
 		policy.setUsername(username);
-		policy.setPuchaseDate(new Date());
-		return insuranceRepo.save(policy);		
+		policy.setPurchaseDate(new Date());
+		this.walletService.updateBalance(username, policy.getPremium());
+		return this.insuranceRepo.save(policy);		
 	}
 	
 	public List<Insurances> InsuranceHistory(String username) {
-		return insuranceRepo.findAllByUsername(username);
+		return this.insuranceRepo.findAllByUsername(username);
 	}
 	
-	public Date getDateOfPurchaseByInsurancename(String insuranceName, String username) {
-		return insuranceRepo.findByUsernameAndInsuranceName(username, insuranceName);
+	public Date getDateOfPurchaseByInsurancename(String username, String insuranceName) {
+		if(this.insuranceRepo.findDateByUsernameAndInsuranceName(username, insuranceName) == null) {
+			log.info("Date not found with the username : " + username + "and insuranceName : " + insuranceName);
+		}
+		return this.insuranceRepo.findDateByUsernameAndInsuranceName(username, insuranceName);
 	}
 
 }
